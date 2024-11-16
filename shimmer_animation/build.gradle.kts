8383988b448ee8490971dad3fff626f44319e64c
Zaminali <zaminali.rustamov@azintelecom.az>
2024-11-16 12:18:01 +0400

    Publish

--- shimmer_animation/build.gradle.kts
+++ shimmer_animation/build.gradle.kts
@@ -51,7 +51,7 @@ afterEvaluate{
             create<MavenPublication>("maven") {
                 groupId = "com.github.zaminalirustamov"
                 artifactId = "shimmer_animation"
-                version = "1.0.0"
+                version = "1.0.2"
                 from(components["release"])
             }
         }
