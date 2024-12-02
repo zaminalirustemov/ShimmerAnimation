04980b921bf97f9f1558dbf53c5a1ee44b5a8164
Zaminali <zaminali.rustamov@azintelecom.az>
2024-12-02 12:02:03 +0400

    Formating

--- app/src/main/java/az/lahza/shimmereffect/adapter/ShimmerAdapter.kt
+++ app/src/main/java/az/lahza/shimmereffect/adapter/ShimmerAdapter.kt
@@ -6,20 +6,21 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import az.lahza.shimmereffect.databinding.ShimmerItemDataBinding
 
-class ShimmerAdapter(private val itemCount: Int) : RecyclerView.Adapter<ShimmerAdapter.ShimmerViewHolder>() {
+class ShimmerAdapter(private val itemCount: Int) :
+    RecyclerView.Adapter<ShimmerAdapter.ShimmerViewHolder>() {
 
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShimmerViewHolder {
-    val view = ShimmerItemDataBinding.inflate(
-        LayoutInflater.from(parent.context),
-        parent,
-        false
-    )
-    return ShimmerViewHolder(view.root)
-  }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShimmerViewHolder {
+        val view = ShimmerItemDataBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ShimmerViewHolder(view.root)
+    }
 
-  override fun onBindViewHolder(holder: ShimmerViewHolder, position: Int) {}
+    override fun onBindViewHolder(holder: ShimmerViewHolder, position: Int) {}
 
-  override fun getItemCount(): Int = itemCount
+    override fun getItemCount(): Int = itemCount
 
-  inner class ShimmerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class ShimmerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
